e840f8936b5cfd0eed1e82ab839d94993207b3cc
Đào Kế Bình <kebinh1235@gmail.com>
2021-11-15 00:06:00 +0700

    upx7

---
+++ .history/assets/scripts/app_20211114193249.js
@@ -0,0 +1,89 @@
+$('.banner__outside-slider').slick(
+  {
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 2000
+  }
+);
+$('.banner__list').slick(
+    {
+        autoplay: true,
+        autoplaySpeed: 2000,
+        prevArrow:"<button type='button' class='slick-prev slick-arrow'><i class='fa fa-angle-left' aria-hidden='true'></i></button>",
+        nextArrow:"<button type='button' class='slick-next slick-arrow'><i class='fa fa-angle-right' aria-hidden='true'></i></button>",
+        responsive: [
+            {
+              breakpoint: 1023,
+              settings: {
+                arrows: false,
+              }
+            },
+          ]
+    }
+);
+$('.product__slider').slick(
+  {
+      autoplay: true,
+      autoplaySpeed: 3000,
+      prevArrow:"<button type='button' class='slick-prev slick-arrow'><i class='fa fa-angle-left' aria-hidden='true'></i></button>",
+      nextArrow:"<button type='button' class='slick-next slick-arrow'><i class='fa fa-angle-right' aria-hidden='true'></i></button>",
+      slidesToShow: 5,
+      slidesToScroll: 5,
+      responsive: [
+        {
+          breakpoint: 1023,
+          settings: {
+            arrows: false,
+            slidesToShow: 2,
+            slidesToScroll: 2,
+            autoplay: false,
+          }
+        },
+      ]
+  }
+);
+$('.tabs__slider').slick(
+  {
+      autoplay: true,
+      autoplaySpeed: 2000,
+      prevArrow:"<button type='button' class='slick-prev slick-arrow'><i class='fa fa-angle-left' aria-hidden='true'></i></button>",
+      nextArrow:"<button type='button' class='slick-next slick-arrow'><i class='fa fa-angle-right' aria-hidden='true'></i></button>",
+      slidesToShow: 4,
+      slidesToScroll: 4,
+      responsive: [
+        {
+          breakpoint: 1023,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2,
+          }
+        },
+      ]
+  }
+);
+$('.brands__slider').slick(
+  {
+      arrows: false,
+      slidesToShow: 3,
+      slidesToScroll: 3,
+      responsive: [
+        {
+          breakpoint: 1023,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2,
+          }
+        },
+      ]
+  }
+);
+$('.app__slider').slick(
+  {
+    autoplay: true,
+    autoplaySpeed: 3000,
+    prevArrow:"<button type='button' class='slick-prev slick-arrow'><i class='fa fa-angle-left' aria-hidden='true'></i></button>",
+    nextArrow:"<button type='button' class='slick-next slick-arrow'><i class='fa fa-angle-right' aria-hidden='true'></i></button>",
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  }
+);
